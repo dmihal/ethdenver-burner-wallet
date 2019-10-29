33eea101c8a421751c1ccde9c4324bfb98a7dbee
David Mihal <david@dmihal.com>
2019-10-29 15:54:38 -0700

    Create wallet repo

---
+++ basic-wallet/src/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import { xdai, dai, eth } from '@burner-wallet/assets';
+import BurnerCore from '@burner-wallet/core';
+import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
+import { InfuraGateway, InjectedGateway, XDaiGateway, } from '@burner-wallet/core/gateways';
+import Exchange, { Uniswap, XDaiBridge } from '@burner-wallet/exchange';
+import ModernUI from '@burner-wallet/modern-ui';
+import LegacyPlugin from '@burner-wallet/legacy-plugin';
+
+const core = new BurnerCore({
+  signers: [new InjectedSigner(), new LocalSigner()],
+  gateways: [
+    new InjectedGateway(),
+    new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
+    new XDaiGateway(),
+  ],
+  assets: [xdai, dai, eth],
+});
+
+const exchange = new Exchange({
+  pairs: [new XDaiBridge(), new Uniswap('dai')],
+});
+
+const BurnerWallet = () =>
+  <ModernUI
+    title="ETHWaterloo"
+    core={core}
+    plugins={[exchange, new LegacyPlugin()]}
+  />
+
+
+
+ReactDOM.render(<BurnerWallet />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
