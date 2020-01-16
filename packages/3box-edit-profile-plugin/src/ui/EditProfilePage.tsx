00d7500462f3a6c0665772b246e1667a7bb12dcf
Kenzo <presto12321@gmail.com>
2020-01-16 10:25:53 -0500

    cleanup and use create box

--- packages/3box-edit-profile-plugin/src/ui/EditProfilePage.tsx
+++ packages/3box-edit-profile-plugin/src/ui/EditProfilePage.tsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect, Component } from 'react';
 import { PluginPageContext } from '@burner-wallet/types';
+import styled from 'styled-components';
 import EditProfile from '3box-profile-edit-react';
 import Box from '3box';
+
 import ThreeBoxEditProfilePlugin from '../ThreeBoxEditProfilePlugin';
 
-// interface EditProfileProps {
-//   box: object;
-//   space: object;
-//   currentUserAddr: string;
-// }
-
-// class EditProfileComponent extends Component<EditProfileProps, {}>;
+const EditProfileContainer = styled.div`
+  position: absolute;
+  left: -16px;
+  top: -40px;
+`;
 
 const EditProfilePage: React.FC<PluginPageContext> = ({ defaultAccount, BurnerComponents, plugin }) => {
   const _plugin = plugin as ThreeBoxEditProfilePlugin;
@@ -23,21 +23,21 @@ const EditProfilePage: React.FC<PluginPageContext> = ({ defaultAccount, BurnerCo
   useEffect(() => {
     async function handleLoad3Box() {
       const addresses = await window.ethereum.enable();
-      const myAddress = addresses[0];
-
-      const updatedBox = await Box.openBox(myAddress, window.ethereum, {});
+      const address = addresses[0];
+      const updatedBox = await Box.create(address, window.ethereum, {});
+      const spaces = ['ethDenver'];
+      await updatedBox.auth(spaces, { address })
       // const updatedBox = await Box.openBox(defaultAccount, _plugin.getProvider(), {});
 
-      const updatedProfile = await Box.getProfile(myAddress);
+      const updatedProfile = await Box.getProfile(address);
       // const updatedProfile = await Box.getProfile(defaultAccount);
 
       const updatedSpace = await updatedBox.openSpace('ethDenver');
-
       await updatedBox.syncDone;
 
       setSpace(updatedSpace);
       setProfile(updatedProfile);
-      setAddress(myAddress);
+      setAddress(address);
       setBox(updatedBox);
     }
 
@@ -47,12 +47,14 @@ const EditProfilePage: React.FC<PluginPageContext> = ({ defaultAccount, BurnerCo
   return (
     <Page title="Edit profile">
       {box ? (
-        <EditProfile
-          box={box}
-          space={space}
-          currentUserAddr={myAddress}
-        // currentUserAddr={defaultAccount}
-        />
+        <EditProfileContainer>
+          <EditProfile
+            box={box}
+            space={space}
+            currentUserAddr={myAddress}
+            currentUser3BoxProfile={profile}
+          />
+        </EditProfileContainer>
       ) : <p>Loading</p>}
     </Page>
   );
