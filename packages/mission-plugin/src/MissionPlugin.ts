0c500cf7e2a6c99d8cf6044e8b588c67f01be99c
David Mihal <david@dmihal.com>
2020-02-03 14:00:55 -0500

    Poll for new missions

--- packages/mission-plugin/src/MissionPlugin.ts
+++ packages/mission-plugin/src/MissionPlugin.ts
@@ -2,15 +2,27 @@ import { BurnerPluginContext, Plugin } from '@burner-wallet/types';
 import hardcodedMissions from './hardcodedMissions';
 import Mission from './Mission';
 
-export default class MissionPlugin implements Plugin {
-  private url?: string;
+interface Options {
+  refreshSeconds?: number;
+}
 
-  constructor(url?: string) {
+export default class MissionPlugin implements Plugin {
+  private context?: BurnerPluginContext;
+  private url?: string;
+  private addedMissions: { [title: string]: boolean } = {};
+  private refreshSeconds: number;
+
+  constructor(url?: string, { refreshSeconds = 5 }: Options = {}) {
     this.url = url;
+    this.refreshSeconds = refreshSeconds;
   }
 
-  async initializePlugin(pluginContext: BurnerPluginContext) {
-    const addMission = (mission: Mission) => pluginContext.addButton(`floor_${mission.floor}`, mission.title, null, {
+  addMission(mission: Mission) {
+    if (this.addedMissions[mission.title]) {
+      return;
+    }
+
+    this.context.addButton(`floor_${mission.floor}`, mission.title, null, {
       description: mission.task,
       logo: mission.image,
       xp: mission.xp,
@@ -19,17 +31,28 @@ export default class MissionPlugin implements Plugin {
       color: mission.color,
     });
 
+    this.addedMissions[mission.title] = true;
+  }
+
+  async initializePlugin(pluginContext: BurnerPluginContext) {
+    this.context = pluginContext;
 
     for (const mission of hardcodedMissions) {
-      addMission(mission);
+      this.addMission(mission);
     }
 
+    this.loadDynamicMissions();
+
+    setInterval(() => this.loadDynamicMissions(), this.refreshSeconds * 1000);
+  }
+
+  async loadDynamicMissions() {
     if (this.url) {
       const response = await fetch(this.url);
       const json = await response.json();
 
       for (const mission of json) {
-        addMission(mission);
+        this.addMission(mission);
       }
     }
   }
