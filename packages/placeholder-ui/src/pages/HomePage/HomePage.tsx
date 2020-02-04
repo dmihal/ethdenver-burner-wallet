1c28b62dd04c318741ba63204ffccf0ec0245e14
David Mihal <david@dmihal.com>
2020-02-04 18:37:56 -0500

    placeholder - Sign overrider

--- packages/placeholder-ui/src/pages/HomePage/HomePage.tsx
+++ packages/placeholder-ui/src/pages/HomePage/HomePage.tsx
@@ -8,17 +8,36 @@ const HomePage: React.FC = () => {
   const [status, setStatus] = useState('fortmatic');
   const { accounts, actions } = useBurner();
 
+  const isOverride = () => {
+    const isContractWallet = actions.canCallSigner('isContractWallet', accounts[0]);
+    if (!isContractWallet) {
+      console.log('Are contract wallets disabled?');
+      return true;
+    }
+
+    const localSigner = accounts[accounts.length - 1]
+    return actions.callSigner('getSignerOverride', accounts[0]) === localSigner;
+  }
+
   useEffect(() => {
     if (!actions.callSigner('user', 'fortmatic')) {
       setStatus('fortmatic');
       return;
     }
+
   }, [accounts]);
+
+  const login = async () => {
+    await actions.callSigner('enable', 'fortmatic');
+    if (!isOverride()) {
+      await actions.callSigner('setSignerOverride', accounts[0], accounts[accounts.length - 1]);
+    }
+  };
 
   return (
     <ScrollingGame>
       {status === 'fortmatic' && (
-        <button onClick={() => actions.callSigner('enable', 'fortmatic').then(() => setStatus(''))}>
+        <button onClick={login}>
           Sign in with Fortmatic
         </button>
       )}
